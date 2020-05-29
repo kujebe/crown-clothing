cb6dcd45c78ecae47b357557db1b00a44a6fa3a1
kujebe <kujebeseun@gmail.com>
2020-05-29 18:20:31 +0200

    Base setup, and home page initial design

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./homepage.component";
+
+function App() {
+  console.log("Hello");
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
