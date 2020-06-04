ff6c616f65054d6d6445b02e6a8af25e55d91c8e
kujebe <kujebeseun@gmail.com>
2020-06-04 20:25:49 +0100

    Added reselect to app, updated cart redux selectors to memoized selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { auth } from "firebase/firebase.utils";
-import CartIcon from "components/cart-icon/cart-icon";
+import CartIcon from "components/cart-icon/cart-icon.component";
 import CartDropdown from "components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "assets/logo.svg";
 
